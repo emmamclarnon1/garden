da0a65fe91ec5eab3a204824d98f07f2516b0a97
Emma McLarnon <emma.mclarnon@hmrc.gsi.gov.uk>
2019-01-27 21:20:12 +0000

    new post

--- plant harvest calc.R
+++ plant harvest calc.R
@@ -11,18 +11,18 @@ sown=c(today)
 harv=(today+months(6))
 h2=today-years(1)
 harvest=c(harv,harv,h2)
-plants=data.frame(veg,sown,harvest)
-names(plants)=c("Vegetable", "Date_Sown", "Harvest_Date")
+plants_1=data.frame(veg,sown,harvest)
+names(plants_1)=c("Vegetable", "Date_Sown", "Harvest_Date")
 
-str(plants)
+str(plants_1)
 
 
 
 library(DT)
-plants2=plants%>%
+plants_2=plants_1%>%
   mutate(ready=case_when(Harvest_Date >= today() ~ "Not yet", TRUE~"Harvest"))
     
-datatable(plants2) %>% formatStyle("ready",
+datatable(plants_2) %>% formatStyle("ready",
   backgroundColor = styleEqual(c("Not yet", "Harvest"), c('white', 'yellow'))
 )
 
