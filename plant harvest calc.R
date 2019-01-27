e6cea8956885fdcf52cdd6b6b6f04da0063b0cc7
Emma McLarnon <emma.mclarnon@hmrc.gsi.gov.uk>
2019-01-27 15:34:42 +0000

    testing new page

--- plant harvest calc.R
+++ plant harvest calc.R
@@ -1,12 +1,29 @@
 library(lubridate)
+library(tidyverse)
+#install.packages("DT")
+
+today="2019-01-16"
+today=ymd(today)
 
 #Make dataframe
 veg=c("Onions", "Garlic", "Asparagus")
-sown=c(today(),today(),today())
-harv=(today()+months(6))
-h2=today()+years(3)
+sown=c(today)
+harv=(today+months(6))
+h2=today-years(1)
 harvest=c(harv,harv,h2)
 plants=data.frame(veg,sown,harvest)
-names(plants)=c("Vegetable", "Date Sown", "Harvest Date")
+names(plants)=c("Vegetable", "Date_Sown", "Harvest_Date")
 
 str(plants)
+
+
+
+library(DT)
+plants2=plants%>%
+  mutate(ready=case_when(Harvest_Date >= today() ~ "Not yet", TRUE~"Harvest"))
+    
+datatable(plants2) %>% formatStyle("ready",
+  backgroundColor = styleEqual(c("Not yet", "Harvest"), c('white', 'yellow'))
+)
+
+
